d491c968f02a1a7f1df12864024268b3e72b404c
amos zohar <azamoszohar@gmail.com>
2019-03-30 21:06:30 +0300

    changes geteAliases to handle a large payload to write

--- services/getAliases.js
+++ services/getAliases.js
@@ -1,7 +1,17 @@
-module.exports = getAliases = (str) => {
-	let aliases = [];
-	for(let i = str.length-1; i >=0 ; i--){
-		aliases.push(str.substr(0,str.length-i));
-	}
-	return aliases;
+// module.exports = getAliases = (str) => {
+// 	let aliases = [];
+// 	for(let i = str.length-1; i >=0 ; i--){
+// 		aliases.push(str.substr(0,str.length-i));
+// 	}
+// 	return aliases;
+// }
+let aliases = [];
+module.exports = getAliases = (str, i = str.length-1) => {
+	if(i>=0){
+        aliases.push(str.substr(0,str.length-i));
+        setImmediate(getAliases,--i);
+    }
+	else{
+        return aliases; 
+    }
 }

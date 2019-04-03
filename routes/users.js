1314ce9b9c44ce98a19ff2abb4537e8cd151b3e8
amos zohar <azamoszohar@gmail.com>
2019-04-04 00:27:50 +0300

    added more validation

--- routes/users.js
+++ routes/users.js
@@ -11,16 +11,20 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/:name', (req, res, next) => {
-  let [name,password] = req.params.name.split('&');
+  let [name,password] = req.params.name.split('&');//TODO: pass validation to a MW, since all requests of this nature must recieve the same treatment
   password = MD5(password);
   delete req.params.name;//kill any trace of unencrypted password
   read(whereToCollectionName, name)
     .then(returnValue => {
       let matchinguser = returnValue[0];
+      if(matchinguser.isLocked){
+        return res.json({msg:`account locked. a message was sent to ${matchinguser.email}`})
+      }
       if (matchinguser.password === password) {
         return res.json([{ name: matchinguser.name, image: matchinguser.image }]);//return only the necessary bits. for now, name and image
       }
-      throw new Error('Error: incorrect password');
+      //TODO: Here send request to change numberOfFails, and if > 4 than set to lock. do it smart
+      return res.json({msg:'wrong password'})
     })
     .catch(err => res.send(err));
 });
